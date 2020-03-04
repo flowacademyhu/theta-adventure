93f2a7a736baed3174f71269221454a3c45a45a9
SutyaM <sutya.miklos.cv@gmail.com>
2020-03-04 14:57:25 +0100

    items in progress

--- draw.js
+++ draw.js
@@ -1,10 +1,11 @@
 const map = require('./maps.js');
+const inventory = require('./inventory.js')
 
 const player = {coords: coords = {
                 x: [24, 25, 24, 25],
                 y: [14, 14, 15, 15]
                                 }
-              }
+                }
 
 /* const board = [
   [0, 0, 0, 0, 0],
@@ -34,7 +35,7 @@ const player = {coords: coords = {
 
 // setInterval(drawv1, 500, board, player);
 
-const isPlayerCord = (x, y, player) =>{
+const isPlayerCord = (x, y, player) => {
   for (let k = 0; k < player.coords.x.length; k++) {
     if (x === player.coords.x[k] && y === player.coords.y[k]) {
       return true;
@@ -43,13 +44,24 @@ const isPlayerCord = (x, y, player) =>{
   return false;
 }
 
-const draw = (board, player) => {
+const isKey1Cord = (x, y, key1) => {
+  for (let k = 0; k < key1.coords.x.length; k++) {
+    if (x === key1.coords.x[k] && y === key1.coords.y[k]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const draw = (board, player, key1) => {
   console.clear();
   let line = "";
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (isPlayerCord(i, j, player) === true) {
         line += ' *';
+      } else if (isKey1Cord(i, j, key1) === true) {
+        inventory.keyCoord();
       } else if (board[i][j] === 1) {
         line += ' 1';
       } else {
@@ -59,9 +71,11 @@ const draw = (board, player) => {
     line += '\n';
   }
   console.log(line);
+  console.log('Your inventory:')
+  console.log(Object.values(inventory.inventory));
 }
 
-setInterval(draw, 50, map.castle, player);
+setInterval(draw, 50, map.castle, player, inventory.key1);
 
 const stdin = process.stdin;
 stdin.setRawMode(true);
@@ -92,3 +106,8 @@ stdin.on('data', (key) => {
     process.exit(0);
   }
 });
+
+module.exports = {
+  isPlayerCord: isPlayerCord,
+  draw: draw
+}
