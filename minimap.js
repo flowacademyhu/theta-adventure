6285f2f07ebf39efc5c8e79de3d40b9a5b2f6c3a
Youngcatlady95 <szizitaa@gmail.com>
2020-03-05 20:37:52 +0100

    minimap 1.0

---
+++ minimap.js
@@ -0,0 +1,12 @@
+const maps = require('./mapreader');
+
+const minimap = [
+  ['undef', 'undef', 'openBottom.txt', 'undef', 'undef'],
+  ['lab1.txt', 'lab2.txt', 'openUpBottom.txt', 'undef', 'undef'],
+  ['lab3.txt', 'lab4.txt', 'castle.txt', 'garden4.txt', 'dragonpit.txt'],
+  ['undef', 'garden2.txt', 'garden.txt', 'garden3.txt', 'undef'],
+  ['openRight.txt', 'ghostcastle.txt']
+];
+module.exports = {
+  minimap: minimap
+}
