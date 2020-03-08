80578fd0e3ec6eb6226a28549b5a182af4315fc5
Atis89 <fejerattila89@gmail.com>
2020-03-08 15:14:31 +0100

    random move

--- random_enemy_move.js
+++ random_enemy_move.js
@@ -8,10 +8,10 @@ const enemy = {
 const enemyRandomMove = (enemy, map) => {
   enemy.coords.x[0] = Math.random(Math.map);
   enemy.coords.x[0]++;
-  if (enemy.coords.x[0])
-  enemy.coords.y[0] = Math.random(Math.map);
-  enemy.coords.y[0]++;
-
+  if (enemy.coords.x[0]) {
+    enemy.coords.y[0] = Math.random(Math.map);
+    enemy.coords.y[0]++;
+  }
 };
 
 setInterval(enemy, 1000);
